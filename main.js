794fd32417f10840a547227da2f16a4ef4ceab08
alone5325V <sthaajesh5@gmail.com>
2026-01-17 10:06:31 +0545

    fixed javascript
    made code flexible

--- main.js
+++ main.js
@@ -1,39 +1,22 @@
-import {propertyForSaleArr} from './properties/propertyFolSaleArr.js'
-import {placeholderPropertyObj} from './properties/placeholderPropertyObj.js'
+import {propertyForSaleArr} from './properties/propertyForSaleArr.js'
+import { placeholderPropertyObj } from './properties/placeholderPropertyObj.js'
 
-function getPropertyHtml() {
-    const htmlStrings = propertyForSaleArr.map(({propertyLocation, priceGBP, comment, roomsM2, image}) => {
+function getPropertyHtml(propertyArr = [placeholderPropertyObj]){
+    return propertyArr.map(property => {
+        const {propertyLocation, priceGBP, roomsM2, comment, image} = property
+        const RoomSize = roomsM2.reduce((acc, curr) => acc + curr )
         return `
             <section class="card">
-            <img src="/images/${image}">
+                <img src = "/images/${image}>
                 <div class="card-right">
-                <h2>${propertyLocation}</h2>
-                <h3>${priceGBP}</h3>
-                <p>${comment}</p>
-                <h3>${getTotalSize(roomsM2)}</h3>
-            </div>
+                    <h2>${propertyLocation}</h2>
+                    <h3>$ ${priceGBP}</h3>
+                    <p>${comment}</p>
+                    <h3>${RoomSize} m&sup2;</h3>
+                </div>
             </section>
         `
     })
-        const elseHtmlStrings = placeholderPropertyObj.map(({propertyLocation, priceGBP, comment, roomsM2, image}) => {
-           return `
-            <section class="card">
-            <img src="/images/${image}">
-                <div class="card-right">
-                <h2>${propertyLocation}</h2>
-                <h3>${priceGBP}</h3>
-                <p>${comment}</p>
-                <h3>${getTotalSize(roomsM2)}</h3>
-            </div>
-            </section>
-        ` 
-        })
 }
 
-function getTotalSize(meter){
-    return meter.reduce((acc, curr) => {
-        return acc + curr
-    },0)
-}
-
-document.getElementById('container').innerHTML = getPropertyHtml()
+document.getElementById('container').innerHTML = getPropertyHtml(propertyForSaleArr)
